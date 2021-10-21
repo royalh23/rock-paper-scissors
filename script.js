394c49860a100a46519683720a971eb2381ef8cc
Royal <royalhuseyn902@gmail.com>
2021-10-21 18:47:41 +0900

    Create the computerPlay and playRound functions.

---
+++ script.js
@@ -0,0 +1,26 @@
+function computerPlay() {
+  const gameElements = ['Rock', 'Paper', 'Scissors'];
+  return gameElements[Math.floor((Math.random()* gameElements.length))];
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+    return "You win! Rock beats Scissors.";
+  } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+      return "You lose! Rock beats Scissors.";
+  } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+      return "You win! Paper beats Rock.";
+  } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+      return "You lose! Paper beats Rock.";
+  } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+      return "You win! Scissors beats Paper.";
+  } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+    return "You lose! Scissors beats Paper.";
+  } else {
+      return "Please enter a valid object.";
+  }
+}
+
+const computerSelection = computerPlay();
+let playerSelectionOriginal = prompt('Please enter an object.').toLowerCase();
+const playerSelection = playerSelectionOriginal.charAt(0).toUpperCase() + playerSelectionOriginal.slice(1);
