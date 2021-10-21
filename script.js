e699eca3ce9ba979156452da6cc17ee4d98d7011
Royal <royalhuseyn902@gmail.com>
2021-10-21 21:26:34 +0900

    Add some comments.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+// Return a random object for computer
 function computerPlay() {
   const gameElements = ['Rock', 'Paper', 'Scissors'];
   return gameElements[Math.floor((Math.random()* gameElements.length))];
 }
 
+// Play a round of game
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
     return "You win! Rock beats Scissors.";
@@ -22,5 +24,7 @@ function playRound(playerSelection, computerSelection) {
 }
 
 const computerSelection = computerPlay();
-let playerSelectionOriginal = prompt('Please enter an object.').toLowerCase();
+const playerSelectionOriginal = prompt('Please enter an object.').toLowerCase()
 const playerSelection = playerSelectionOriginal.charAt(0).toUpperCase() + playerSelectionOriginal.slice(1);
+console.log(playRound(playerSelection, computerSelection));
+
