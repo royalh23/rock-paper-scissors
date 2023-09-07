81c653b5179e226036b9d998718985e989b86d83
Royal <royalhuseyn902@gmail.com>
2023-09-07 21:03:02 +0400

    Indent some comment-code blocks for further clarification

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function getComputerChoice() {
   let gameItems = ["Rock", "Paper", "Scissors"];
-  // get a random number between 0, 1, and 2. 
+
+  // Get a random number between 0, 1, and 2
   let randomIndexNumber = Math.floor((Math.random() * gameItems.length));
-  // then use one of this random numbers as an index to get a random array value
+
+  // Then use one of this random numbers as an index to get a random array value
   return gameItems[randomIndexNumber];
+}
+
+function playRound(playerSelection, computerSelection) {
+
+  
 }
